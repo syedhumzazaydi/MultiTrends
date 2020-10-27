68301904bd6aab7fd219c9a1559755ae000133eb
syedhumzazaydi <70915607+syedhumzazaydi@users.noreply.github.com>
2020-10-27 23:09:55 +0500

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,84 @@
+﻿var resultJSON = {
+    "rdlReportsList": [
+        {
+            "reportid": 15,
+            "reportTitle": "Dependent drill 1"
+        },
+        {
+            "reportid": 14,
+            "reportTitle": "Dependent Drill Report"
+        },
+        {
+            "reportid": 13,
+            "reportTitle": "Dependent report"
+        },
+        {
+            "reportid": 12,
+            "reportTitle": "Funding Report Test"
+        },
+        {
+            "reportid": 17,
+            "reportTitle": "Invoice Summary"
+        },
+        {
+            "reportid": 16,
+            "reportTitle": "PODetail"
+        },
+        {
+            "reportid": 18,
+            "reportTitle": "Test Report drill"
+        }
+    ],
+    "adhocReportsList": [
+        {
+            "reportid": 32,
+            "reportTitle": "CanViewSteve"
+        },
+        {
+            "reportid": 9,
+            "reportTitle": "Emran Adhoc"
+        },
+        {
+            "reportid": 34,
+            "reportTitle": "HEX"
+        },
+        {
+            "reportid": 33,
+            "reportTitle": "newwd"
+        },
+        {
+            "reportid": 13,
+            "reportTitle": "Reycle Report"
+        },
+        {
+            "reportid": 22,
+            "reportTitle": "TEST Permis"
+        },
+        {
+            "reportid": 31,
+            "reportTitle": "Testing Final"
+        },
+        {
+            "reportid": 27,
+            "reportTitle": "Testing Hamzas Work"
+        },
+        {
+            "reportid": 30,
+            "reportTitle": "TETTTT"
+        }
+    ],
+    "linkReportsList": [
+        {
+            "reportid": 120078,
+            "reportTitle": "Outside Bullseye",
+            "reportUrl": "http://bemv2.awesometechinc.com",
+            "isEmbedded": false
+        },
+        {
+            "reportid": 120077,
+            "reportTitle": "Inside Bullseye",
+            "reportUrl": "http://calibrateuat.eustismortgage.com/",
+            "isEmbedded": true
+        }
+    ]
+};
